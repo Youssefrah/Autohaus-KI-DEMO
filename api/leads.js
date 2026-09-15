f7c6100d8041a8aa0fe29853d2b85ef900487adc
Youssefrah <youssefrahime9@gmail.com>
2026-09-15 23:38:58 +0200

    Update leads.js

--- api/leads.js
+++ api/leads.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         from: "onboarding@resend.dev",
-        to: ["to:delivered@resend.dev"],
+        to: to: ["delivered@resend.dev"],
         subject: `🚗 Neue Probefahrt-Anfrage: ${lead.vehicle || "Fahrzeug"}`,
         html: `
           <h2>🚗 Neue Probefahrt-Anfrage</h2>
